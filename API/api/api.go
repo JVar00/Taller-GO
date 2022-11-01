fbe1311cd47e94b29aa54a39857d496702921f74
JeffVar00 <jeff.vargas.barrantes@est.una.ac.cr>
2022-10-31 21:07:46 -0600

    Actividad

--- API/api/api.go
+++ API/api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"API/data"
-	"API/models"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -10,74 +9,12 @@ import (
 
 type Api struct{}
 
-type TasksParams struct {
-	Limit int `query:"limit"`
-	//echo utiliza el parametro query para poder mapear estos keys values.
-}
-
 type TaskIdParams struct {
 	Id int `param:"id"`
 }
 
 func (a *Api) getTasks(e echo.Context) error {
-
-	params := &TasksParams{}
-
-	err := e.Bind(params)
-
-	if err != nil {
-		return e.JSON(http.StatusBadRequest, "Invalid Query Params")
-	}
-
-	if params.Limit < 0 || params.Limit > len(data.TasksData) {
-		return e.JSON(http.StatusBadRequest, "Invalid Query Params")
-	}
-
-	var to int
-
-	if params.Limit > 0 {
-		to = params.Limit
-	} else {
-		to = len(data.TasksData)
-	}
-
-	return e.JSON(http.StatusOK, data.TasksData[:to])
-}
-
-func (a *Api) postTask(e echo.Context) error {
-
-	var task models.Task
-
-	err := e.Bind(&task)
-
-	if err != nil {
-		return e.JSON(http.StatusBadRequest, "Invalid parameters")
-	}
-
-	data.TasksData = append(data.TasksData, task)
-	return e.NoContent(http.StatusCreated)
-}
-
-func (a *Api) deleteTask(e echo.Context) error {
-
-	params := &TaskIdParams{}
-
-	err := e.Bind(params)
-	if err != nil {
-		return e.JSON(http.StatusBadRequest, "Invalid ID")
-	}
-
-	index := params.Id
-
-	for i, task := range data.TasksData {
-		if task.ID == index {
-			// [a, b, c] esto concatena los los indices 0 y 2 y elimina el indice 1 (b)
-			data.TasksData = append(data.TasksData[:i], data.TasksData[i+1:]...)
-			return e.JSON(http.StatusOK, index)
-		}
-	}
-
-	return e.JSON(http.StatusNotFound, "Unexisting ID")
+	return e.JSON(http.StatusOK, data.TasksData)
 }
 
 func (a *Api) getTask(e echo.Context) error {
@@ -99,32 +36,3 @@ func (a *Api) getTask(e echo.Context) error {
 
 	return e.JSON(http.StatusNotFound, "Unexisting ID")
 }
-
-//ESTO SE ELIMINA AL INICIO DE LA ACTIVIDAD, SE MANTIENE PARA REPASO PARA EXPLICAR EL FUNCIONAMIENTO DEL UPDATETASK
-
-// func (a *Api) updateTask(e echo.Context) error {
-
-// 	index, err := strconv.Atoi(e.Param("id"))
-
-// 	var updatedTask models.Task
-
-// 	if err != nil {
-// 		return e.JSON(http.StatusBadRequest, "Invalid ID")
-// 	}
-
-// 	err = e.Bind(&updatedTask)
-// 	if err != nil {
-// 		return e.JSON(http.StatusBadRequest, "Invalid Content")
-// 	}
-
-// 	for i, task := range data.TasksData {
-// 		if task.ID == index {
-// 			data.TasksData = append(data.TasksData[:i], data.TasksData[i+1:]...)
-// 			updatedTask.ID = task.ID
-// 			data.TasksData = append(data.TasksData, updatedTask)
-// 			return e.JSON(http.StatusOK, index)
-// 		}
-// 	}
-
-// 	return e.JSON(http.StatusNotFound, "Unexisting ID")
-// }
--- API/api/routes.go
+++ API/api/routes.go
@@ -10,12 +10,5 @@ func (a *Api) RegisterRoutes(e *echo.Echo) {
 	e.GET("/tasks", a.getTasks)
 
 	e.GET("/tasks/:id", a.getTask)
-	e.DELETE("/tasks/:id", a.deleteTask)
-	//el :id es un parametro que recibimos
-
-	e.POST("/tasks", a.postTask)
 
 }
-
-//ESTO SE ELIMINA AL INICIO DE LA ACTIVIDAD
-//e.PUT("/tasks/:id", a.updateTask)
